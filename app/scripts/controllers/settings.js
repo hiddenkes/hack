148aad06b544acbc87f61c2084be2d4afb452e99
rogerbutt <andrew.luu.93@gmail.com>
2014-04-13 09:45:03 -0500

    Added settings page

--- app/scripts/controllers/settings.js
+++ app/scripts/controllers/settings.js
@@ -2,7 +2,13 @@
 
 angular.module('hackApp')
   .controller('SettingsCtrl', function ($scope, $http) {
-    $http.get('/api/awesomeThings').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+
+    $scope.pace = function(speed) {
+
+    };
+
+    $scope.save = function() {
+
+    };
+
   });
